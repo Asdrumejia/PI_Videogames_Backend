478916f44eaaff9c9c45b902ab117f0426943488
Asdrúbal Mejía López <asdrumejia19@gmail.com>
2023-01-26 21:13:10 -0500

    api/src/models(Videogame models updated)

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,12 +1,59 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "videogame",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      released: {
+        type: DataTypes.DATEONLY,
+      },
+
+      rating: {
+        type: DataTypes.FLOAT,
+        validate: {
+          min: 1.0,
+          max: 5,
+        },
+      },
+
+      platforms: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: true,
+      },
+
+      background_image: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+
+      createdInDb: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
     },
-  });
+    {
+      timestamps: false,
+    }
+  );
 };
